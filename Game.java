733cd9c056a99bdab0701b0b002863c727f0eb06
Vitty97 <alvarezgonzalrgmail.com>
2019-06-05 20:11:29 +0200

    Commit 12

--- Game.java
+++ Game.java
@@ -145,8 +145,8 @@ public class Game
         System.out.println("You are lost. You are alone. You wander");
         System.out.println("around at the university.");
         System.out.println();
-        System.out.println("Your command words are:");
-        System.out.println("   go quit help");
+        System.out.println("Los comandos validos son: ");
+        parser.showCommands();
     }
 
     /** 
